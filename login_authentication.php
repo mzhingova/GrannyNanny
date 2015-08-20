e066079d9eaad748a0d18f9d5261d253d6e128dd
linaNikolova <tsvetelina.nikolova@gmail.com>
2015-08-20 18:00:41 +0300

    update

--- login_authentication.php
+++ login_authentication.php
@@ -1,49 +1,38 @@
 <?php
 session_start();
-?>
-<?php
 $pageTitle = 'Log-in';
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
 $conn = mysqli_connect('localhost', 'root', '', 'grannynanny');
 if (!$conn) {
 	die('Could not connect: ' . mysql_error());
 	exit;
 }
-
 if (isset($_POST['email']) and isset($_POST['pass'])) {
-	$email = $_POST['email'];
-	$password = $_POST['pass'];
-
-	$query = mysqli_query($conn, "SELECT * FROM parenuser WHERE email='$email' AND pass='$password'") or die(mysql_error());
+	$email = htmlentities($_POST['email']);
+	$password = htmlentities($_POST['pass']);
+	$escapedPassword = mysqli_real_escape_string($conn, $password);
+	$escapedEmail = mysqli_real_escape_string($conn, $email);
+	$query = mysqli_query($conn, "SELECT * FROM parenuser WHERE email='$escapedEmail' AND pass='$escapedPassword'") or die("Стана грешкка " . mysql_error());
 	$count = mysqli_num_rows($query);
 	if (!$query) {
 		echo mysqli_error($conn);
 	}
-
 	if ($count == 1) {
 		while ($row = $query->fetch_assoc()) {
 			$_SESSION["name"] = $row['firstname'];
 			$_SESSION["lastname"] = $row['lastname'];
 			$_SESSION["status"] = $row['status'];
-			/*if ($_SESSION["status"]=="nanny"){
-			header('Location: user_profile.php');
+			if ($_SESSION["status"] == "nanny") {
+				header('Location: user_profile.php');
+			} else if ($_SESSION["status"] == "user") {
+				header('Location: user_profile.php');
+			} else if ($_SESSION["status"] == "admin") {
+				header('Location: user_profile.php');
 			}
-			else if ($_SESSION["status"]=="user"){
-			header('Location: user_profile.php');
-			}
-			else if ($_SESSION["status"]=="admin"){*/
-			header('Location: profile.php');
-			// }
 		}
-
 	} else {
 		header('Location: error.php');
 		//echo "Invalid Login Credentials.";
 	}
 }
-
-?>
-
