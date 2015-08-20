2c537b59d0453b67636c5a0e5b58eee370c6f0a8
mzhingovaq <m.zhingova@gmail.com>
2015-08-20 16:53:48 +0300

    footer edit

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,9 @@
+<p>Следвайте ни в:</p>
+<a href="https://twitter.com/"><img class="social_media" title="Twitter" alt="Twitter" src="assets/img/twitter.png"/></a>
+<a href="https://facebook.com"><img class="social_media" title="Facebook" alt="Facebook" src="assets/img/facebook.png"/></a>
+<a href="https://plus.google.com/"><img class="social_media"  alt="GooglePlus" src="assets/img/google.png"/></a>
 <?php
+
 echo "<footer><h3>Copyright &copy; 1910-" . date("Y") . " GrannyNanny.com</h3></footer>";
 ?>
+	
