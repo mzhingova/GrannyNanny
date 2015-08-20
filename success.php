5c4f0c14252e8cf14644c994b544f81a839b7949
mzhingovaq <m.zhingova@gmail.com>
2015-08-20 16:22:50 +0300

    success registration css

--- success.php
+++ success.php
@@ -4,15 +4,27 @@
 		<meta charset="utf-8">
 		<title>Successful Registration</title>
 		<!-- Override CSS file - add your own CSS rules -->
-		<link rel="stylesheet" href="assets/css/main_style.css">
+		
 	</head>
 	<body>
-		<?php include 'includes/header.php';?>
-		<div class="content">
+		
+		
+		<p class="main">
 			<div class="container">
-				<h2>Успешна регистрация</h2>
-			</div>
+				<?php include 'includes/header.php';?>
+				<div class="content">
+					<div class="container">
+						<div class="col1">
+							<center><h2>Успешна регистрация!</h2></center><br>
+						</div></p>
+					</div>
+				</div>
 		</div>
-		<?php include 'includes/footer.php';?>
+			<div class="container">
+				<?php include 'includes/footer.php';?>
+			</div>
+			
+		</div>
+		
 	</body>
 </html>
