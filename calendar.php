d3921513aace6062c062e3b52da339cbff31e0ae
BoyanKatsarski <bojankacarski@gmail.com>
2015-08-20 16:48:05 +0300

    Added under construction text for non created pages

--- calendar.php
+++ calendar.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>Home Page</title>
+		<title>Calendar</title>
 		<!-- Override CSS file - add your own CSS rules -->
 		<link rel="stylesheet" href="assets/css/###.css">
 	</head>
@@ -10,6 +10,7 @@
 		<div class="container">
 			<?php include 'includes/header.php';?>
 			<div class="content">
+						<?php echo "The page you are looking for is under construction" ?>
 						<div class="container">
 				<div class="content">
 					<?php include 'includes/footer.php';?>
