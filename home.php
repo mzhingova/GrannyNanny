b4a0cc59d74ad7dc33e4c26b09403248a33811ae
mzhingovaq <m.zhingova@gmail.com>
2015-08-21 16:19:04 +0300

    after test push

--- home.php
+++ home.php
@@ -18,55 +18,55 @@ if (!$conn) {
 		<meta charset="utf-8">
 		<title>Profile Page</title>
 		<link rel="stylesheet" href="assets/css/home_style.css">
-	</head>
-	<body>
-		
+			</head>
+			<body>
+				
 	
 			<div class="container">
 			<?php include 'includes/header.php';?>
-			<div class="welcome">	<?php echo "Hello " . ($_SESSION["name"]) . " " . ($_SESSION["lastname"]) . " welcome to your Home page!";?> </div>
+			<h1 class="welcome">	<?php echo "Hello " . ($_SESSION["name"]) . " " . ($_SESSION["lastname"]) . " welcome to your Home page!";?> </h1>
 				
 
 
-<?php if(isset($_SESSION['status']) && ($_SESSION['status']=="admin")) {
+			<?php if(isset($_SESSION['status']) && ($_SESSION['status']=="admin")) {
 
 
-?>
-<div class="count"> <?php
-$queryUser = $conn->prepare("SELECT status COUNT FROM parenuser WHERE status='user'");
+			?>
+			<div class="count"> <?php
+			$queryUser = $conn->prepare("SELECT status COUNT FROM parenuser WHERE status='user'");
 
-if (!$queryUser) {
-		echo mysqli_error($conn);
-	}
+			if (!$queryUser) {
+					echo mysqli_error($conn);
+				}
 
-$queryUser->execute();
-$queryUser->store_result();
-$rows = $queryUser->num_rows;
+			$queryUser->execute();
+			$queryUser->store_result();
+			$rows = $queryUser->num_rows;
 
-echo "Общ брой регистрирани потребители: ". $rows;
+			echo "Общ брой регистрирани потребители: ". $rows;
 
-?> </div>
-<div class="count"> <?php
-$queryNanny = $conn->prepare("SELECT status COUNT FROM parenuser WHERE status='nanny'");
-if (!$queryNanny) {
-		echo mysqli_error($conn);
-	}
-$queryNanny->execute();
-$queryNanny->store_result();
-$rows = $queryNanny->num_rows;
+			?> </div>
+			<div class="count"> <?php
+			$queryNanny = $conn->prepare("SELECT status COUNT FROM parenuser WHERE status='nanny'");
+			if (!$queryNanny) {
+					echo mysqli_error($conn);
+				}
+			$queryNanny->execute();
+			$queryNanny->store_result();
+			$rows = $queryNanny->num_rows;
 
-echo "Общ брой регистрирани nannies: ". $rows;
-?></div>
+			echo "Общ брой регистрирани nannies: ". $rows;
+			?></div>
 
 
 
-<div><img src="assets/img/chart.jpg" alt="chart"></div>
-<?php } ?>
+			<div><img class="chart"" src="assets/img/chart.jpg" alt="chart"></div>
+			<?php } ?>
 
-		</div>
-		</div>
-		<div class="container">
-				<?php include 'includes/footer.php';?>
-			</div>
-	</body>
-</html>
+					</div>
+					</div>
+					<div class="container">
+							<?php include 'includes/footer.php';?>
+						</div>
+				</body>
+			</html>
