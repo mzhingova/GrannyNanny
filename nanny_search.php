8795417f296975f34070f813f587104faafb4109
linaNikolova <tsvetelina.nikolova@gmail.com>
2015-08-21 17:49:26 +0300

    top 5 nannies done

--- nanny_search.php
+++ nanny_search.php
@@ -1,3 +1,18 @@
+<?php 
+$pageTitle = 'Log-in';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+$conn = new mysqli('localhost', 'root', '', 'grannynanny');
+
+if (!$conn) {
+	die('Could not connect: ' . mysql_error());
+	exit;
+}
+$conn ->set_charset("utf8");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -10,7 +25,13 @@
 		<div class="container">
 			<?php include 'includes/header.php';?>
 			<div class="content">
-				<h1>Топ 5 Nanny</h1>
+				<h1>Топ 5 Nannies</h1>
+					<?php 
+					$tableQuery = mysqli_query($conn, "SELECT * FROM parenuser where status='nanny' LIMIT 5")or die("Стана грешкка " . mysql_error());; 
+					$query = mysqli_query($conn, "SELECT * FROM parenuser WHERE status='nanny'") or die("Стана грешкка " . mysql_error());
+					
+					while($row = mysqli_fetch_array($tableQuery)) { ?>					
+					<br>
 					<table>
 					<tbody>
 						<tr>
@@ -19,27 +40,37 @@
 							</td>
 							<td>
 								Име:
-								<?php> <?>
+								<?php 
+								echo $row['firstname']. " ". $row['lastname']; ?>
+								
 							</td>
 						</tr>
 						<tr>
 							<td>
-								Години:
+								Години: 
+								<?php if($row['pid'] != '') {
+									echo date('Y')-(intval($row['pid']/100000000) + 1900);
+									} else 
+									{
+										echo '-'; 
+										} ?>
 							</td>
 						</tr>
 						<tr>
 							<td>
-								Град:
+								Град: 
+								<?php echo $row['city']; ?>
 							</td>
 						</tr>
 						<tr>
 							<td>
 								Описание:
+								<?php echo $row['motivation']; ?>
 							</td>
 						</tr>
 						<tr>
 							
-							<?php if(isset($_SESSION['status']) && ($_SESSION['status']=="user")){?>
+							<?php if(isset($_SESSION['status']) && ($_SESSION['status']=="user")){ ?>
 			<td>
 							<button class="btn">Ангажирай</button>
 							</td>
@@ -47,6 +78,11 @@
 						</tr>
 					</tbody>
 				</table>
+
+<?php } ?>
+
+
+			</div>
 			</div>
 			<div class="container">
 				<?php include 'includes/footer.php';?>
