eae3cf3f84354e0a17cabb40159c508b2f3b238b
BoyanKatsarski <bojankacarski@gmail.com>
2015-08-21 17:45:54 +0300

    added new class to a div

--- pages.php
+++ pages.php
@@ -11,8 +11,22 @@
 		<div class="container">
 			<?php include 'includes/header.php';?>
 			<div class="content">
-			<div><button class="btn"></button></div>
-				<?php echo "Страницата \"Преглед на всички страници\" е все още under construction"; ?>
+			
+			<div class="newButton"><a href="about_us.php"><button class="btnA">About Us</button></div></a>
+			<div class="newButton"><a href="apply_for_nanny.php"><button class="btnA">Apply for Nanny</button></div></a>
+			<div class="newButton"><a href="calendar.php"><button class="btnA">Calendar</button></div></a>
+			<div class="newButton"><a href="congratulations.php"><button class="btnA">Congratulations</button></div></a>
+			<div class="newButton"><a href="forgotten_password.php"><button class="btnA">Forgotten Password</button></div></a>
+			<div class="newButton"><a href="help.php"><button class="btnA">Help</button></div></a>
+			<div class="newButton"><a href="home_page.php"><button class="btnA">Home Page</button></div></a>
+			<div class="newButton"><a href="index.php"><button class="btnA">Index</button></div></a>
+			<div class="newButton"><a href="login.php"><button class="btnA">Login</button></div></a>
+			<div class="newButton"><a href="messages.php"><button class="btnA">Messages</button></div></a>
+			<div class="newButton"><a href="nannies.php"><button class="btnA">Nannies</button></div></a>
+			<div class="newButton"><a href="nanny_profil.php"><button class="btnA">Nanny Profile</button></div></a>
+			<div class="newButton"><a href="nanny_reg.php"><button class="btnA">Nanny Registration</button></div></a>
+			<div class="newButton"><a href="nanny_search.php"><button class="btnA">Nanny Search</button></div></a>
+			<div class="newButton"><a href="profile.php"><button class="btnA">Profile</button></div></a>
 			</div>
 			<div class="container">
 				<?php include 'includes/footer.php';?>
