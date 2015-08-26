27f5424fcbaf2b0a76f5a0d9f441c9a33208ab95
BoyanKatsarski <bojankacarski@gmail.com>
2015-08-26 15:33:18 +0300

    created user edit page

---
+++ updateUser.php
@@ -0,0 +1,64 @@
+<?php
+ session_start();
+ ?>
+
+<?php 
+$pageTitle = 'Log-in';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$conn = new mysqli('localhost', 'root', '', 'grannynanny');
+
+if (!$conn) {
+    die('Could not connect: ' . mysql_error());
+    exit;
+}
+$conn ->set_charset("utf8");
+?>
+
+
+
+
+<?php
+$userID=$_SESSION['userID'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$address = $_POST['address'];
+$tel = $_POST['tel'];
+$city = $_POST['city'];
+
+
+
+
+if(!empty($firstname))
+{
+    mysqli_query($conn, "UPDATE parenuser SET `firstname`='$firstname' WHERE userID='$userID'") or die(mysql_error());
+                echo("You have successfully updated your First name");
+} 
+if(!empty($lastname))
+{
+    mysqli_query($conn, "UPDATE parenuser SET `lastname`='$lastname' WHERE userID='$userID'") or die(mysql_error());
+                echo("You have successfully updated your Last name");
+} 
+if(!empty($address))
+{
+    mysqli_query($conn, "UPDATE parenuser SET `address`='$address' WHERE userID='$userID'") or die(mysql_error());
+                echo("You have successfully updated your Address");
+} 
+if(!empty($tel))
+{
+    mysqli_query($conn, "UPDATE parenuser SET `tel`='$tel' WHERE userID='$userID'") or die(mysql_error());
+                echo("You have successfully updated your Telephone");
+} 
+ 
+if(!empty($city))
+{
+    mysqli_query($conn, "UPDATE parenuser SET `city`='$city' WHERE userID='$userID'") or die(mysql_error());
+                echo("You have successfully updated your City");
+} 
+
+
+
+
+?>
+
