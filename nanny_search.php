23a3dd455c6d78fddf5a031a52ff2c6e03721192
mzhingovaq <m.zhingova@gmail.com>
2015-08-28 11:13:42 +0300

    pagination update

--- nanny_search.php
+++ nanny_search.php
@@ -27,7 +27,23 @@ $conn ->set_charset("utf8");
 			<div class="content">
 				<h1>Топ 5 Nannies</h1>
 					<?php 
-					$tableQuery = mysqli_query($conn, "SELECT * FROM parenuser where status='nanny' LIMIT 5")or die("Стана грешкка " . mysql_error()); 
+					$per_page=4;
+
+						if (isset($_GET['page'])) {
+
+						$page = $_GET['page'];
+						}
+						else {
+						$page=1;
+						}
+						$start_from = ($page-1) * $per_page;					
+					
+					
+					
+					
+					
+					
+					$tableQuery = mysqli_query($conn, "SELECT * FROM parenuser where status='nanny' LIMIT $start_from, $per_page")or die("Стана грешкка " . mysql_error()); 
 					$query = mysqli_query($conn, "SELECT * FROM parenuser WHERE status='nanny'") or die("Стана грешкка " . mysql_error());
 					
 					while($row = mysqli_fetch_array($tableQuery)) { ?>					
@@ -81,7 +97,28 @@ $conn ->set_charset("utf8");
 				</table>
 
 <?php } ?>
+<?php
 
+//Now select all from table
+$query = "SELECT * FROM parenuser WHERE status='nanny'";
+$result = mysqli_query($conn, $query);
+
+// Count the total records
+$total_records = mysqli_num_rows($result);
+
+//Using ceil function to divide the total records on per page
+$total_pages = ceil($total_records / $per_page);
+
+//Going to first page
+echo "<a href='nanny_search.php?page=1'>".'First Page'."</a> ";
+
+for ($i=1; $i<=$total_pages; $i++) {
+
+echo "<a href='nanny_search.php?page=".$i."'>".$i."</a> ";
+};
+// Going to last page
+echo "<a href='nanny_search.php?page=$total_pages'>".'Last Page'."</a> ";
+?>
 
 			</div>
 			</div>
