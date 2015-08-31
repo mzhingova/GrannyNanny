da0bddcbe6187a9f7439bdb36cd7f5b190dae523
Martin Gyurov <martin.gyurov89@gmail.com>
2015-08-31 15:25:44 +0300

    forgotten password

--- about_us.php
+++ about_us.php
@@ -7,11 +7,75 @@
 		<link rel="stylesheet" href="assets/css/###.css">
 	</head>
 	<body>
+		<script>
+				function toggleElement(id)
+{
+    if(document.getElementById(id).style.display == 'none')
+    {
+        document.getElementById(id).style.display = '';
+    }
+    else
+    {
+        document.getElementById(id).style.display = 'none';
+    }
+}
+</script>
+
+
 		<div class="container">
 			<?php include 'includes/header.php';?>
 			<div class="content">
-				<?php echo "The page you are looking for is under construction" ?>
-				
+
+<h1>WHY THE ‘ABOUT US’ PAGE ROCKS:</h1>
+
+<p>
+<a href="javascript:toggleElement('a1')">1.It tells us a story.</a>
+</p>
+<div id="a1" style="display:none">
+Every company has a story to tell, so break out your storytelling
+skills from that random English class you took years
+ago and put them to work on your ‘About Us’ page. Using
+descriptive and emotive copy, an ‘About Us’ page with a
+story works harder for your business than a generic one.<br><br>
+</div>
+<p>
+<a href="javascript:toggleElement('a2')">2.It shows some personality.</a>
+</p>
+<div id="a2" style="display:none">
+For your ‘About Us’ page, you don’t need to pretend to be a
+comedian if that isn’t part of your brand’s personality. Find
+another aspect of your brand’s personality to showcase and
+make a lasting impression by being yourself.<br><br>
+</div>
+<p>
+<a href="javascript:toggleElement('a3')">3.It’s human.</a>
+</p>
+<div id="a3" style="display:none">
+When you’re whipping up your ‘About Us’ page, make sure
+to avoid industry jargon and instead use your authentic voice
+to describe your product or service. Sure, it needs to be polished
+and free of errors, but it should always sound friendly
+and real.<br><br>
+</div>
+<p>
+<a href="javascript:toggleElement('a4')">4.It lets customers do the talking.</a>
+</p>
+<div id="a4" style="display:none">
+Personal testimonials build credibility and trust, which is
+why they have a natural home on your ‘About Us’ page. If
+you have a customer who has nothing but good things to say
+about your company, why not hand them the mic and let
+them do the talking?<br><br>
+</div>
+<p>
+<a href="javascript:toggleElement('a5')">5.It skips the business babble.</a>
+</p>
+<div id="a5" style="display:none">
+The moral of the story here is that you should try to get
+rid of jargon on your ‘About Us’ page whenever possible.
+Show your smarts through simple copy -- not business
+babble<br><br>
+</div>
 			</div>
 		</div>
 		<div class="container">
