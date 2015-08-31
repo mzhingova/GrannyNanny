5b64a8d8395cd58b213ba20686bc4c613166a7cc
mzhingovaq <m.zhingova@gmail.com>
2015-08-31 10:51:27 +0300

    style edit

--- user-search-result.php
+++ user-search-result.php
@@ -81,7 +81,7 @@ if (isset($_REQUEST['search-button'])) {
 					echo "<button class='btn'>Ангажирай</button>";
 					echo "</div>";
 				} else {
-					echo "<div class='btn'>";
+					echo "<div >";
 					echo "<a class='btn' href='edit_user.php?id=$key->userID'>Редактирай</a>";
 					echo "</div>";
 				}
@@ -130,7 +130,7 @@ if (isset($_REQUEST['search-button'])) {
 						echo "<button class='btn'>Ангажирай</button>";
 						echo "</div>";
 					} else {
-						echo "<div class='btn'>";
+						echo "<div >";
 						echo "<a class='btn' href='edit_user.php?id=$key->userID'>Редактирай</a>";
 						echo "</div>";
 					}
