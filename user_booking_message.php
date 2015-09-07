d0bb34c9aa9d075d170081b0b3c08c7afa059e4c
linaNikolova <tsvetelina.nikolova@gmail.com>
2015-09-07 12:03:00 +0300

    booking requests pages update

--- user_booking_message.php
+++ user_booking_message.php
@@ -67,23 +67,7 @@ if (isset($_SESSION['status']) && ($_SESSION['status'] == "user")){
 			</div>
 		</div>
 		
-	<?php } ?>
-	<?php	
-		if (isset($_GET['action'])){
-			$book_id = $_GET['id'];
-			if($_GET['action']=="reject"){
-				$update_status = mysqli_query($conn, "UPDATE booking SET status='rejected' WHERE bookingID = '$book_id'")or die("Стана грешкка " . mysql_error()); 
-				if($update_status){
-					header("Refresh: 1; url=home_page.php");
-				}
+	<?php }
 				
-			}
-			if($_GET['action']=="accept"){
-				$update_status = mysqli_query($conn, "UPDATE booking SET status='accepted' WHERE bookingID = '$book_id'")or die("Стана грешкка " . mysql_error()); 
-				if($update_status){
-					header("Refresh: 1; url=home_page.php");
-				}
-			}
-		}			
 	}
  ?>
