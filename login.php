21d1878d12c598521792a813086dce2e949f6212
BoyanKatsarski <bojankacarski@gmail.com>
2015-09-08 17:44:47 +0300

    fixed some minor issues

--- login.php
+++ login.php
@@ -14,12 +14,15 @@
 				<form action='login_authentication.php' method='post'>
 					<font color="black">
 					<h3>Имейл адрес: </h3><input type='text' name='email' style="width: 25%; height: 10%;" /><br>
-					<h3>Парола: </h3><input type='password' name='pass' style="width: 25%; height: 10%;" /><br><br>
+					<h3>Парола: </h3><input type='password' name='pass' style="width: 25%; height: 10%;" />
 					<button id="btn" type="submit" name="submit" class="btn">Вход</button><br><br>
+					<a href="registration.php" >
+						 Натиснете тук за Регистрация!</a> <br>
+					<a href="forgotten_password.php">Натиснете тук за Забравена Парола</a><br><br>
+					
+					
 
-					<a href="forgotten_password.php">Забравена Парола</a><br><br>
-					Нямате акаунт?<a href="registration.php" >
-						 Регистрирайте се!</a>
+					
 						</font>
 					</form>
 				</div>
