3d51c5c27c58d3d0502ba9058753c3cc2e6fdeb0
linaNikolova <tsvetelina.nikolova@gmail.com>
2015-09-11 11:43:07 +0300

    UI fix

--- error.php
+++ error.php
@@ -4,17 +4,16 @@
 		<meta charset="utf-8">
 		<title>Error Page</title>
 		<!-- Override CSS file - add your own CSS rules -->
-		<link rel="stylesheet" href="assets/css/main_style.css">
 	</head>
 	<body>
 			<div class="container">
 				<?php include 'includes/header.php';?>
-				<div class="content">
-				<h2>Невалиден e-mail или парола!</h2>
+					<div class="content">
+						<center><h2>Невалиден e-mail или парола!</h2></center><br>
+					</div>
 			</div>
-		</div>
 		<div class="container">
 				<?php include 'includes/footer.php';?>
-			</div>
+		</div>
 	</body>
-</html
+</html>
--- error_booking.php
+++ error_booking.php
@@ -4,18 +4,17 @@
 		<meta charset="utf-8">
 		<title>Error Page</title>
 		<!-- Override CSS file - add your own CSS rules -->
-		<link rel="stylesheet" href="assets/css/main_style.css">
+
 	</head>
 	<body>
 			<div class="container">
 				<?php include 'includes/header.php';?>
 				<div class="content">
-
-				<h2>Възникна проблем, моля опитайте отново!</h2>
+					<center><h2>Възникна проблем, моля опитайте отново!</h2></center><br>
+				</div>
 			</div>
-		</div>
 		<div class="container">
 				<?php include 'includes/footer.php';?>
-			</div>
+		</div>
 	</body>
-</html
+</html>
