bfdc00697a8644408117159a4455007528afd88b
BoyanKatsarski <bojankacarski@gmail.com>
2015-09-11 11:08:59 +0300

    pages page

--- pages.php
+++ pages.php
@@ -11,22 +11,18 @@
 		<div class="container">
 			<?php include 'includes/header.php';?>
 			<div class="content">
-			
-			<div class="newButton"><a href="about_us.php"><button class="btn">About Us</button></div></a>
-			<div class="newButton"><a href="apply_for_nanny.php"><button class="btn">Apply for Nanny</button></div></a>
-			<div class="newButton"><a href="calendar.php"><button class="btn">Calendar</button></div></a>
-			<div class="newButton"><a href="congratulations.php"><button class="btn">Congratulations</button></div></a>
-			<div class="newButton"><a href="forgotten_password.php"><button class="btn">Forgotten Password</button></div></a>
-			<div class="newButton"><a href="help.php"><button class="btn">Help</button></div></a>
-			<div class="newButton"><a href="home_page.php"><button class="btn">Home Page</button></div></a>
-			<div class="newButton"><a href="index.php"><button class="btn">Index</button></div></a>
-			<div class="newButton"><a href="login.php"><button class="btn">Login</button></div></a>
-			<div class="newButton"><a href="messages.php"><button class="btn">Messages</button></div></a>
-			<div class="newButton"><a href="nannies.php"><button class="btn">Nannies</button></div></a>
-			<div class="newButton"><a href="nanny_profil.php"><button class="btn">Nanny Profile</button></div></a>
-			<div class="newButton"><a href="nanny_reg.php"><button class="btn">Nanny Registration</button></div></a>
-			<div class="newButton"><a href="nanny_search.php"><button class="btn">Nanny Search</button></div></a>
-			<div class="newButton"><a href="profile.php"><button class="btn">Profile</button></div></a>
+			<div class="newButton"><a href="index.php"><button class="btn">Index</button></a> - Стартовата страница на приложението</div>
+			<div class="newButton"><a href="apply_for_nanny.php"><button class="btn">Apply For Nanny</button></a> - Кандидатствай за Nanny</div>
+			<div class="newButton"><a href="search.php"><button class="btn">Nanny Search</button></a> - Търси Nanny</div>
+			<div class="newButton"><a href="search_one.php"><button class="btn">User Search</button></a> - Търси Потребител</div>
+			<div class="newButton"><a href="help.php"><button class="btn">Help</button></a> - Помощ</div>
+			<div class="newButton"><a href="about_us.php"><button class="btn">About Us</button></a> - За Нас</div>
+			<div class="newButton"><a href="registration.php"><button class="btn">Registration</button></a> - Регистрация за потребител</div>
+			<div class="newButton"><a href="login.php"><button class="btn">Login</button></a> - Логни се</div>
+			<div class="newButton"><a href="home_page.php"><button class="btn">Home Page</button></a> - Страница с заявки</div>
+			<div class="newButton"><a href="calendar.php"><button class="btn">Calendar</button></a> - Календар</div>
+			<div class="newButton"><a href="forgotten_password.php"><button class="btn">Forgotten Password</button></a> - Забравена Парола</div>
+			<div class="newButton"><a href="nanny_reg.php"><button class="btn">Nanny Registration</button></a> - Регистрирай Nanny</div>
 			</div>
 			<div class="container">
 				<?php include 'includes/footer.php';?>
