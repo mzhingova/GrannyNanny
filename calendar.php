4f3e5b23179e555e34a6553c576dd48206512ffa
BoyanKatsarski <bojankacarski@gmail.com>
2015-09-17 17:33:54 +0300

    ss

--- calendar.php
+++ calendar.php
@@ -5,13 +5,24 @@
 		<title>Calendar</title>
 		<!-- Override CSS file - add your own CSS rules -->
 		<link rel="stylesheet" href="assets/css/###.css">
+
 	</head>
 	<body>
 		<div class="container">
 			<?php include 'includes/header.php';?>
 			<div class="content">
-						<?php echo "The page you are looking for is under construction" ?>
-						</div>
+
+
+
+
+
+
+
+
+
+
+
+							</div>
 						</div>
 				<div class="container">
 				<?php include 'includes/footer.php';?>
